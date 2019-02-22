41bcc2ee0eaa247a9db0e72a3b54179cede17c59
nromano7 <nick.p.romano@gmail.com>
2019-02-22 01:35:34 -0500

    update docker config for launching on ec2

--- src/2-init-db.bat
+++ src/2-init-db.bat
@@ -1,5 +1,8 @@
 @echo on
 
-docker run --name srs-dbinit --rm nromano7/srs:demo -m elastic.index
+docker run --name srs-dbinit --rm ^
+--link elasticsearch:elasticsearch ^
+--env ELASTICSEARCH_URL=http://elasticsearch:9200 ^
+nromano7/srs:demo -m elastic.index
 
 pause
